b5ca3c59944ef913a8ea54b84416c11b45fba0ec
Arman170616 <armanice09@gmail.com>
2022-11-07 09:14:20 +0600

    first commit

---
+++ socalproject/urls.py
@@ -0,0 +1,14 @@
+
+
+from django.contrib import admin
+from django.urls import path, include
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
+
+urlpatterns = [
+    path('', home),
+    path("admin/", admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+]
